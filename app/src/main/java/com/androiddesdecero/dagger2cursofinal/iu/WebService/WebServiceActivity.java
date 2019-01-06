d84fb88dd0aa624ee187b78524cbc888c2b1f637
Alberto <info@androiddesdecero.com>
2019-01-06 10:15:23 +0100

    13

---
+++ app/src/main/java/com/androiddesdecero/dagger2cursofinal/iu/WebService/WebServiceActivity.java
@@ -0,0 +1,15 @@
+package com.androiddesdecero.dagger2cursofinal.iu.WebService;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androiddesdecero.dagger2cursofinal.R;
+
+public class WebServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_service);
+    }
+}
--- app/src/main/java/com/androiddesdecero/dagger2cursofinal/iu/profile/ProfileActivity.java
+++ app/src/main/java/com/androiddesdecero/dagger2cursofinal/iu/profile/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.androiddesdecero.dagger2cursofinal.iu.profile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.androiddesdecero.dagger2cursofinal.R;
+import com.androiddesdecero.dagger2cursofinal.di.BaseApp;
+import com.androiddesdecero.dagger2cursofinal.iu.WebService.WebServiceActivity;
+import com.androiddesdecero.dagger2cursofinal.model.User;
 
-public class ProfileActivity extends AppCompatActivity {
+import javax.inject.Inject;
+
+public class ProfileActivity extends AppCompatActivity implements Profile.View {
 
     private EditText etName;
     private EditText etEdad;
@@ -17,12 +23,16 @@ public class ProfileActivity extends AppCompatActivity {
     private Button btNextActivity;
     private Button btUpdate;
 
+    @Inject
+    Profile.Presenter presenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        ((BaseApp)getApplication()).getAppComponent().inject(this);
         setUpView();
+        presenter.setView(this);
     }
 
     private void setUpView(){
@@ -40,7 +50,7 @@ public class ProfileActivity extends AppCompatActivity {
         btNextActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(getApplicationContext(), WebServiceActivity.class));
             }
         });
 
@@ -48,8 +58,17 @@ public class ProfileActivity extends AppCompatActivity {
         btUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                User user = new User();
+                user.setEdad(etEdad.getText().toString());
+                user.setUsername(etName.getText().toString());
+                presenter.updateUser(user);
             }
         });
+    }
+
+    @Override
+    public void showUser(User user) {
+        etEdad.setText(user.getEdad());
+        etName.setText(user.getUsername());
     }
 }
