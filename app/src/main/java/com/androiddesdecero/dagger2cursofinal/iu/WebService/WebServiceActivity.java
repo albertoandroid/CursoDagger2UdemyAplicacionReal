14191586f22ec8a2712a9483df896811b002cb93
Alberto <info@androiddesdecero.com>
2019-01-06 10:28:35 +0100

    15

--- app/src/main/java/com/androiddesdecero/dagger2cursofinal/iu/WebService/WebServiceActivity.java
+++ app/src/main/java/com/androiddesdecero/dagger2cursofinal/iu/WebService/WebServiceActivity.java
@@ -2,14 +2,47 @@ package com.androiddesdecero.dagger2cursofinal.iu.WebService;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.androiddesdecero.dagger2cursofinal.R;
 
+import org.w3c.dom.Text;
+
 public class WebServiceActivity extends AppCompatActivity {
+
+    private Button btVolverAProfile;
+    private Button btHacerPeticionWeb;
+    private TextView tvUserName;
+    private TextView tvEdad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_service);
+
+        setUpView();
+    }
+
+    private void setUpView(){
+        tvUserName = findViewById(R.id.tvUserName);
+        tvEdad = findViewById(R.id.tvEdad);
+
+        btVolverAProfile = findViewById(R.id.btVolverAProfile);
+        btVolverAProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btHacerPeticionWeb = findViewById(R.id.btHacerPeticionWeb);
+        btHacerPeticionWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
