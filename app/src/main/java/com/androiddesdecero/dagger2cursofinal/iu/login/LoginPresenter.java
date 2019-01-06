12bae53378f138b26951421914532f80e011613c
Alberto <info@androiddesdecero.com>
2019-01-06 09:31:04 +0100

    05

--- app/src/main/java/com/androiddesdecero/dagger2cursofinal/iu/login/LoginPresenter.java
+++ app/src/main/java/com/androiddesdecero/dagger2cursofinal/iu/login/LoginPresenter.java
@@ -1,5 +1,7 @@
 package com.androiddesdecero.dagger2cursofinal.iu.login;
 
+import com.androiddesdecero.dagger2cursofinal.model.User;
+
 /**
  * Created by albertopalomarrobledo on 6/1/19.
  */
@@ -8,8 +10,10 @@ public class LoginPresenter implements Login.Presenter {
 
     private Login.View view;
 
-    public LoginPresenter(){
+    private User user;
 
+    public LoginPresenter(User user){
+        this.user = user;
     }
 
     @Override
@@ -21,6 +25,8 @@ public class LoginPresenter implements Login.Presenter {
     public void validadUser(String userName, String pass) {
         if(view != null){
             if(userName.equals("alberto")&&pass.equals("1234")){
+                user.setUsername("Alberto Palomar");
+                user.setEdad("30");
                 view.usuarioValido();
             }else{
                 view.error();
---
+++ app/src/main/java/com/androiddesdecero/dagger2cursofinal/model/User.java
@@ -0,0 +1,31 @@
+package com.androiddesdecero.dagger2cursofinal.model;
+
+/**
+ * Created by albertopalomarrobledo on 6/1/19.
+ */
+
+public class User {
+
+    private String username;
+    private String edad;
+
+    public User(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+}
