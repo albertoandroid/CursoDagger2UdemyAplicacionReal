2e6a1a84c18bcb1f033a738946828f1c53770ea0
Alberto <info@androiddesdecero.com>
2019-01-06 09:19:51 +0100

    03

---
+++ app/src/main/java/com/androiddesdecero/dagger2cursofinal/iu/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.androiddesdecero.dagger2cursofinal.iu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.androiddesdecero.dagger2cursofinal.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUser;
+    private EditText etPassword;
+    private Button btLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        setUpView();
+    }
+
+    private void setUpView(){
+        etUser = findViewById(R.id.etUser);
+        etPassword = findViewById(R.id.etPassword);
+        btLogin = findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
