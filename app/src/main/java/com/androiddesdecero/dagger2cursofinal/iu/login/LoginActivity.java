8d12d543d42fd6412ed959c6f0e77f9172b014c4
Alberto <info@androiddesdecero.com>
2019-01-06 09:50:26 +0100

    09

--- app/src/main/java/com/androiddesdecero/dagger2cursofinal/iu/login/LoginActivity.java
+++ app/src/main/java/com/androiddesdecero/dagger2cursofinal/iu/login/LoginActivity.java
@@ -1,36 +1,56 @@
 package com.androiddesdecero.dagger2cursofinal.iu.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.androiddesdecero.dagger2cursofinal.R;
+import com.androiddesdecero.dagger2cursofinal.di.BaseApp;
+import com.androiddesdecero.dagger2cursofinal.iu.profile.ProfileActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import javax.inject.Inject;
+
+public class LoginActivity extends AppCompatActivity implements Login.View {
 
     private EditText etUser;
     private EditText etPassword;
     private Button btLogin;
 
+    @Inject
+    Login.Presenter presenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        ((BaseApp)getApplication()).getAppComponent().inject(this);
         setUpView();
     }
 
     private void setUpView(){
+        presenter.setView(this);
         etUser = findViewById(R.id.etUser);
         etPassword = findViewById(R.id.etPassword);
         btLogin = findViewById(R.id.btLogin);
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                presenter.validadUser(etUser.getText().toString(), etPassword.getText().toString());
             }
         });
+    }
+
+    @Override
+    public void usuarioValido() {
+        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+    }
+
+    @Override
+    public void error() {
+        Toast.makeText(this, "Usuario no es Valido", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/androiddesdecero/dagger2cursofinal/iu/profile/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.androiddesdecero.dagger2cursofinal.iu.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androiddesdecero.dagger2cursofinal.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
