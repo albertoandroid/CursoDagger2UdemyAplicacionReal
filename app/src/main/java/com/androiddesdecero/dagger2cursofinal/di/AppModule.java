6ecc0840e1959433949142cc3fd2fda432422d91
Alberto <info@androiddesdecero.com>
2019-01-06 10:52:21 +0100

    19

--- app/src/main/java/com/androiddesdecero/dagger2cursofinal/di/AppModule.java
+++ app/src/main/java/com/androiddesdecero/dagger2cursofinal/di/AppModule.java
@@ -3,16 +3,22 @@ package com.androiddesdecero.dagger2cursofinal.di;
 import android.app.Application;
 import android.content.Context;
 
+import com.androiddesdecero.dagger2cursofinal.api.ApiClient;
 import com.androiddesdecero.dagger2cursofinal.iu.login.Login;
 import com.androiddesdecero.dagger2cursofinal.iu.login.LoginPresenter;
 import com.androiddesdecero.dagger2cursofinal.iu.profile.Profile;
 import com.androiddesdecero.dagger2cursofinal.iu.profile.ProfilePresenter;
 import com.androiddesdecero.dagger2cursofinal.model.User;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by albertopalomarrobledo on 6/1/19.
@@ -47,6 +53,43 @@ public class AppModule {
     @Singleton
     public User provideUser(){
         return new User();
+    }
+
+    /*
+    Grafo de Retrofit
+     */
+    private static final String BASE_URL = "https://api.github.com";
+
+    @Singleton
+    @Provides
+    GsonConverterFactory providesGsonConverterFactory(){
+        GsonConverterFactory gsonConverterFactory = GsonConverterFactory.create();
+        return gsonConverterFactory;
+    }
+
+    @Singleton
+    @Provides
+    OkHttpClient provideOkHttpClient(){
+        return new OkHttpClient.Builder()
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .build();
+    }
+
+    @Singleton
+    @Provides
+    Retrofit provideRetrofit(OkHttpClient client, GsonConverterFactory converterFactory){
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(converterFactory)
+                .client(client)
+                .build();
+    }
+
+    @Singleton
+    @Provides
+    ApiClient provideApiCliente(Retrofit retrofit){
+        return retrofit.create(ApiClient.class);
     }
 
     /*
